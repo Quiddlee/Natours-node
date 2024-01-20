f809030d05861d5190fa11cdc29ea19182faf1ca
Bogdan <bogdanscherbinadev@gmail.com>
2024-01-20 12:42:03 +0200

    feat: connect with mongoDB Atlas

--- server.ts
+++ server.ts
@@ -1,4 +1,24 @@
+import mongoose from 'mongoose';
+
 import app from './app';
+
+import 'dotenv/config';
+
+const DB = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD,
+);
+
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  })
+  .then(() => console.log('DB connection successful!'))
+  .catch((err) =>
+    console.log(`DB connection error is ${(err as Error).message}`),
+  );
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
